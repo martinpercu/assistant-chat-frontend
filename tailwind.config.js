c1530a6aa927881bd5b1633ac8169651c457a182
martinpercu <97993590+martinpercu@users.noreply.github.com>
2025-02-20 19:48:44 -0500

    first implementation in html with dark mode enabled

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ module.exports = {
   ],
   darkMode: 'selector', // Activa el modo oscuro cuando la clase 'dark' está presente
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        neutral: {
+          50: '#f9f9f9',
+          100: '#ececec',
+          200: '#eaddd7',
+          300: '#cdcdcd',
+          400: '#b4b4b4',
+          500: '#9b9b9b',
+          600: '#676767',
+          650: '#424242',
+          700: '#303030',
+          750: '#2f2f2f',
+          800: '#212121',
+          900: '#171717',
+          950: '#0d0d0d'
+        },
+      }
+    },
   },
   plugins: [],
 }
